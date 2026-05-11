c059f716eed66a157d5f17cd50c14ead3ca37627
jdholst <jakedavidholst@hotmail.com>
2026-05-11 12:11:25 -0500

    Defining the core game spec

---
+++ src/game-spec/index.ts
@@ -0,0 +1,10 @@
+export {
+  gameSpecSchema,
+  parseGameSpec,
+  stableIdSchema,
+  type GameSpec,
+  type GameSpecMechanicEntry,
+  type GameSpecObjective,
+  type GameSpecValidationGoal,
+  type StableId,
+} from "./game-spec-schema";
